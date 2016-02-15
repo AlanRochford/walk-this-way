64cca9c68e051e96587da47b8133d1867a39f47b
Alan <alan.rochford@mydit.ie>
2016-02-15 13:50:37 +0000

    adding files

--- app/db.php
+++ app/db.php
@@ -9,8 +9,6 @@ $dbname="events";
 $dbConnection = pg_connect("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass")
 or die("Can't connect to database".pg_last_error());
 
-echo("She's Connecting anyway");
-
 return $dbConnection;
 }
 ?>
